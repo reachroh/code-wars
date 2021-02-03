ca6efa61a125a23be92b847873e342ea67c2e1ba
reachroh <reach.rohinim@gmail.com>
2021-02-03 16:20:23 +0530

    updated[8kyu]Get the mean of an array

--- 8kyu/School.java
+++ 8kyu/School.java
@@ -3,7 +3,7 @@ public class School{
   public static int getAverage(int[] marks){
   int sum = 0;
   for(int i=0; i< marks.length;i++){
-  sum=sum+marks[i];
+  sum+=marks[i];
   
   }
   return (sum/marks.length);
