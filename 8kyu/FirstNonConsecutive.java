319d62d6eb9ac6d34d0ac035b5596072415f9dfc
reachroh <reach.rohinim@gmail.com>
2021-02-04 21:18:19 +0530

    [8kyu]Find the first non-consecutive number

---
+++ 8kyu/FirstNonConsecutive.java
@@ -0,0 +1,12 @@
+class FirstNonConsecutive {
+    static Integer find(final int[] array) {
+      for(int x=1;x<array.length;x++)
+        {
+        if (array[x-1] + 1 != array[x] ) 
+        return array[x];
+      }
+      
+    
+        return null; // whole array is consecutive
+    }
+}
